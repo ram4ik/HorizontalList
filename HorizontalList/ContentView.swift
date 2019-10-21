e1322dda7be9fbcac4c3e71ecae4447437ffc291
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-21 09:13:49 +0300

    init

--- HorizontalList/ContentView.swift
+++ HorizontalList/ContentView.swift
@@ -10,12 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<5) { i in
+                        cardView(img: "img\(i)").padding(6)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct cardView: View {
+    
+    var img = ""
+    var body: some View {
+        VStack {
+            Image(img)
+        }.frame(width: 250, height: 400)
+        .cornerRadius(20)
+        .shadow(radius: 20)
     }
 }
